fcc8e682640efa120f4f263f4276f742c24d1188
sofievdw2002 <sofievanderwesten@hotmail.com>
2024-06-16 01:35:57 +0200

    <Added a function to visualize all trainstations and connections, also edited main to call function>

--- code/visualize/visualize.py
+++ code/visualize/visualize.py
@@ -1,4 +1,7 @@
 from matplotlib import pyplot as plt
+import pandas as pd
+from shapely.geometry import Point, LineString
+import geopandas as gpd
 
 def visualize_station_percentage(graph):
     """
@@ -51,4 +54,72 @@ def histogram_k(k_values):
     plt.ylabel('frequency')
     plt.hist(k_values, bins = 10)
     plt.grid(True)
+    plt.show()
+
+
+def plot_stations_and_connections(stations_file, connections_file, shape_file):
+    """
+    visualizes all train stations and connections in Noord and Zuid Holland
+    """
+
+    stations_df = pd.read_csv(stations_file)
+    connections_df = pd.read_csv(connections_file)
+
+    # geodf for stations
+    geometry = [Point(xy) for xy in zip(stations_df['x'], stations_df['y'])]
+    stations_gdf = gpd.GeoDataFrame(stations_df, geometry=geometry)
+
+    # geodf for connections
+    connections = []
+    for index, row in connections_df.iterrows():
+        station1 = row['station1']
+        station2 = row['station2']
+        distance = row['distance']
+
+        # add connections where both stations have coordinates
+        coordinates = [(stations_gdf.loc[stations_gdf['station'] == station1, 'geometry'].squeeze().x, stations_gdf.loc[stations_gdf['station'] == station1, 'geometry'].squeeze().y),
+                       (stations_gdf.loc[stations_gdf['station'] == station2, 'geometry'].squeeze().x, stations_gdf.loc[stations_gdf['station'] == station2, 'geometry'].squeeze().y)]
+        connections.append((LineString(coordinates), distance))
+
+    # plot Noord- and Zuid-Holland
+    fig, ax = plt.subplots(figsize = (10, 10))
+
+    # shapefile path
+    world = gpd.read_file(shape_file)
+    netherlands = world[world['NAME'] == 'Netherlands']
+    netherlands.plot(ax = ax, color = 'grey')
+
+    # plot train stations
+    stations_gdf.plot(ax = ax, color = 'blue')
+
+    for i, row in stations_gdf.iterrows():
+
+        # display these stationnames on the right of the marker instead of the left to avoid overlap
+        if row['station'] in ['Rotterdam Centraal', 'Amsterdam Centraal', 'Amsterdam Amstel']:
+            plt.annotate(text = row['station'], xy = (row['geometry'].x, row['geometry'].y),
+                         xytext = (5, 5), textcoords = 'offset points', ha = 'left')
+
+        # display Amsterdam Sloterdijk as 'Ams Sl' to avoid overlap
+        elif row['station'] == 'Amsterdam Sloterdijk':
+            plt.annotate(text = 'Ams Sl', xy = (row['geometry'].x, row['geometry'].y),
+                         xytext = (5, 5), textcoords = 'offset points', ha = 'left')
+
+        # plot the rest of the stations to the right
+        else:
+            plt.annotate(text=row['station'], xy=(row['geometry'].x, row['geometry'].y),
+                         xytext = (-5, 5), textcoords = 'offset points', ha = 'right')
+
+    # plot connections
+    for line, distance in connections:
+        gpd.GeoSeries([line]).plot(ax=ax, color='red')
+
+    plt.title('Train stations and connections in Noord- and Zuid-Holland')
+    plt.xlabel('longitude')
+    plt.ylabel('latitude')
+
+    # limits to zoom in on Noord- and Zuid-Holland
+    ax.set_xlim(4.0, 5.1)
+    ax.set_ylim(51.75, 53.1)
+
+    plt.tight_layout()
     plt.show()
--- main.py
+++ main.py
@@ -3,13 +3,12 @@ import argparse
 from code.visualize import visualize as vis
 from code.algorithms import main_loop
 
-
-
 def main(input1, input2, output_file, iterations):
 
     # perform  iterations and get k-values and graph
     lst_k_values, test_graph = main_loop.main_loop(input1, input2, iterations)
 
+    vis.plot_stations_and_connections(input1, input2, 'data/ne_10m_admin_0_countries/ne_10m_admin_0_countries.shp')
     vis.bar_k(lst_k_values)
     vis.histogram_k(lst_k_values)
     vis.visualize_station_percentage(test_graph)
