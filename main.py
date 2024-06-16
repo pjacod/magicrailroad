8a07c60ec29553c4d1844aa272358add725d2e3f
sofievdw2002 <sofievanderwesten@hotmail.com>
2024-06-16 02:17:06 +0200

    < changed a small big>

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ def main(input1, input2, output_file, iterations):
     # perform  iterations and get k-values and graph
     lst_k_values, test_graph = main_loop.main_loop(input1, input2, iterations)
 
-    vis.plot_stations_and_connections(input1, input2, 'data/ne_10m_admin_0_countries/ne_10m_admin_0_countries.shp')
     vis.bar_k(lst_k_values)
     vis.histogram_k(lst_k_values)
     vis.visualize_station_percentage(test_graph)
+    vis.plot_stations_and_connections(input1, input2, 'data/ne_10m_admin_0_countries/ne_10m_admin_0_countries.shp')
     test_graph.show_routes()
     test_graph.write_output(output_file)
 
