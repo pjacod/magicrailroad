d3f9dd987f7fe7137d49183150760d292595fb39
sofievdw2002 <sofievanderwesten@hotmail.com>
2024-06-16 02:42:33 +0200

    < added a function to plot the programmed route>

--- code/visualize/visualize.py
+++ code/visualize/visualize.py
@@ -123,3 +123,58 @@ def plot_stations_and_connections(stations_file, connections_file, shape_file):
 
     plt.tight_layout()
     plt.show()
+
+def plot_stations_and_routes(stations_file, connections_file, shape_file, routes):
+    """
+    visualizes train stations and the programmed routes.
+    """
+
+    stations_df = pd.read_csv(stations_file)
+    connections_df = pd.read_csv(connections_file)
+
+    # geodf for stations
+    geometry = [Point(xy) for xy in zip(stations_df['x'], stations_df['y'])]
+    stations_gdf = gpd.GeoDataFrame(stations_df, geometry = geometry)
+
+    # plot Noord- and Zuid-Holland
+    fig, ax = plt.subplots(figsize = (10, 10))
+
+    # shapefile path
+    world = gpd.read_file(shape_file)
+    netherlands = world[world['NAME'] == 'Netherlands']
+    netherlands.plot(ax = ax, color = 'grey')
+
+    # plot train stations
+    stations_gdf.plot(ax = ax, color = 'blue')
+
+    for i, row in stations_gdf.iterrows():
+
+        # display these stationnames on the right of the marker instead of the left to avoid overlap
+        if row['station'] in ['Rotterdam Centraal', 'Amsterdam Centraal', 'Amsterdam Amstel']:
+            plt.annotate(text=row['station'], xy=(row['geometry'].x, row['geometry'].y),
+                         xytext=(5, 5), textcoords='offset points', ha='left')
+
+        # display Amsterdam Sloterdijk as 'Ams Sl' to avoid overlap
+        elif row['station'] == 'Amsterdam Sloterdijk':
+            plt.annotate(text='Ams Sl', xy=(row['geometry'].x, row['geometry'].y),
+                         xytext=(5, 5), textcoords='offset points', ha='left')
+
+        # plot the rest of the stations to the right
+        else:
+            plt.annotate(text=row['station'], xy=(row['geometry'].x, row['geometry'].y),
+                         xytext=(-5, 5), textcoords='offset points', ha='right')
+
+    # plot routes
+    for route_number, route in routes.items():
+        route_coords = [(station.location[0], station.location[1]) for station in route.itinerary]
+        route_line = LineString(route_coords)
+        gpd.GeoSeries([route_line]).plot(ax=ax, color='green', linewidth=3, alpha=0.5)
+
+    plt.title('programmed routes in Noord- and Zuid-Holland')
+
+    # limits to zoom in on Noord- and Zuid-Holland
+    ax.set_xlim(4.0, 5.1)
+    ax.set_ylim(51.75, 53.1)
+
+    plt.tight_layout()
+    plt.show()
