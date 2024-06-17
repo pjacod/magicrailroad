5f9f70ad89df21a5c7b50c7020ace34e61b80fd7
knackebrod2 <arneverwaaijen@hotmail.com>
2024-06-17 15:45:13 +0200

    niks

--- main.py
+++ main.py
@@ -6,42 +6,14 @@ from code.algorithms import main_loop
 
 def main(input1, input2, output_file, iterations):
 
-    lst_k_values = []
-
-    for solution in range(iterations):
-        # create a scenario
-        test_graph = graph.Graph(input1, input2)
-
-        # randomly select an amount of routes
-        amount = randomise.amount_routes()
-
-        # add random amoubt of routes to scenario
-        test_graph.add_routes(amount)
-
-        # use randomise to choose itinerary for route
-        randomise.random_routes(test_graph.route_dict)
-
-        # calculate k_value of traject
-        lst_k_values.append(test_graph.calculate_k())
-
-    vis.bar_k(lst_k_values)
-    vis.histogram_k(lst_k_values)
-    #vis.visualize_station_percentage(test_graph)
-
     # perform  iterations and get k-values and graph
     lst_k_values, test_graph = main_loop.main_loop(input1, input2, iterations)
 
     vis.bar_k(lst_k_values)
     vis.histogram_k(lst_k_values)
     vis.visualize_station_percentage(test_graph)
-<<<<<<< HEAD
-    vis.plot_stations_and_connections(input1, input2, 'data/ne_10m_admin_0_countries/ne_10m_admin_0_countries.shp')
-    vis.plot_stations_and_routes(input1, input2, 'data/ne_10m_admin_0_countries/ne_10m_admin_0_countries.shp', test_graph.route_dict)
-
-=======
     vis.plot_connections(input1, input2, 'data/ne_10m_admin_0_countries/ne_10m_admin_0_countries.shp')
     vis.plot_routes(input1, input2, 'data/ne_10m_admin_0_countries/ne_10m_admin_0_countries.shp', test_graph.route_dict)
->>>>>>> cccccb33e223a8de5b8048599b4cd39b97d11331
     test_graph.show_routes()
     test_graph.write_output(output_file)
 
