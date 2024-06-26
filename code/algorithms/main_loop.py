4766a93a245a70ddba47d2ec27c99a1a5a1d6692
knackebrod2 <arneverwaaijen@hotmail.com>
2024-06-26 22:44:56 +0200

    meer domme shit

--- code/algorithms/main_loop.py
+++ code/algorithms/main_loop.py
@@ -1,6 +1,6 @@
 from code.algorithms import randomise
 from code.classes import graph
-
+from code.algorithms import hill_climber as hc
 
 def main_loop(input1, input2, iterations):
     lst_k_values = []
@@ -14,9 +14,9 @@ def main_loop(input1, input2, iterations):
         # add a route to scenario
         test_graph.add_routes(amount)
 
-        randomise.random_routes(test_graph.route_dict)
-
-        #test_graph, k_values = hill_climber.run(500)
+        #randomise.random_routes(test_graph.route_dict)
+        hill_climber = hc.Hillclimber(test_graph)
+        k_values, test_graph = hill_climber.run(500)
 
         # calculate k_value of traject
         lst_k_values.append(test_graph.calculate_k())
